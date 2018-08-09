eb1730d0588e8c882cff0b681970bfc7daf0f05b
nehalad <nlad1@binghamton.edu>
2018-08-09 11:08:13 -0400

    writing output to the file

--- src/TotalHosts.java
+++ src/TotalHosts.java
@@ -5,7 +5,9 @@ import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Map;
+import java.util.Map.Entry;
 
 public class TotalHosts {
     static Map<String, Integer> hm = new HashMap<String, Integer>();
@@ -33,13 +35,22 @@ public class TotalHosts {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		BufferedWriter wr = null;
 		try {
-			BufferedWriter wr = new BufferedWriter(new FileWriter("output.txt"));
-			wr.write(hm);
+			wr = new BufferedWriter(new FileWriter("output.txt"));
+			Iterator<Entry<String, Integer>> itr = hm.entrySet().iterator();
+			while(itr.hasNext()) {
+				Map.Entry<String, Integer> pairs = itr.next();
+				String str = pairs.getKey()+ " "+pairs.getValue();
+				System.out.print(str+"\n");
+				wr.write(str+"\n");
+			}
+			wr.close();
 		} catch (IOException e1) {
 			// TODO Auto-generated catch block
 			e1.printStackTrace();
 		}
+		
 		System.out.println(hm);
 	}
 }
